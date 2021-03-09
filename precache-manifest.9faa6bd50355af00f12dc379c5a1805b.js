0d828ccd4e1a05c2101fd7abcc15798be2ce5fa4
Yunus Akten <yunusakten2@gmail.com>
2021-03-09 14:45:32 +0300

    Updates

---
+++ precache-manifest.9faa6bd50355af00f12dc379c5a1805b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc80c4a38255232641692200de8556a",
+    "url": "/Shopping-Cart/index.html"
+  },
+  {
+    "revision": "6f24c912da8173087654",
+    "url": "/Shopping-Cart/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "c7c93e964938a16d0b62",
+    "url": "/Shopping-Cart/static/css/main.50853b19.chunk.css"
+  },
+  {
+    "revision": "6f24c912da8173087654",
+    "url": "/Shopping-Cart/static/js/2.0348491e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Shopping-Cart/static/js/2.0348491e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7c93e964938a16d0b62",
+    "url": "/Shopping-Cart/static/js/main.8f85833c.chunk.js"
+  },
+  {
+    "revision": "137adc49f576381da53a",
+    "url": "/Shopping-Cart/static/js/runtime-main.dae2d93c.js"
+  }
+]);
